604e0ee8675062a7711230750689bc9491cda05f
Aryaveer667766 <aryaveer0121@gmail.com>
2025-07-15 21:01:50 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// firebase.js
+const firebaseConfig = {
+  apiKey: "AIzaSyCHh9XG4eK2IDYgaUzja8Lk6obU6zxIIwc",
+  authDomain: "fortunespin-57b4f.firebaseapp.com",
+  databaseURL: "https://fortunespin-57b4f-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "fortunespin-57b4f",
+  storageBucket: "fortunespin-57b4f.appspot.com",
+  messagingSenderId: "204339176543",
+  appId: "1:204339176543:web:b417b7a2574a0e44fbe7ea",
+  measurementId: "G-VT1N70H3HK"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
