5858bfdcf57683d3e06e2922d73e10580b2061e6
Aryaveer667766 <aryaveer0121@gmail.com>
2025-07-16 23:30:10 +0530

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,24 @@
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-database.js";
+
+// ✅ Your Firebase config here
+const firebaseConfig = {
+  apiKey: "AIzaSyCHh9XG4eK2IDYgaUzja8Lk6obU6zxIIwc",
+  authDomain: "fortunespin-57b4f.firebaseapp.com",
+  databaseURL: "https://fortunespin-57b4f-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "fortunespin-57b4f",
+  storageBucket: "fortunespin-57b4f.appspot.com",
+  messagingSenderId: "204339176543",
+  appId: "1:204339176543:web:b417b7a2574a0e44fbe7ea",
+  measurementId: "G-VT1N70H3HK"
+};
+
+// 🔧 Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+// ✅ Export to use in all modules
+export { auth, db };
