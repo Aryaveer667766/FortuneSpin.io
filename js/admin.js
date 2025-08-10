9835b321ad5eb3cb532468a09ad2a197e2e6565c
Aryaveer667766 <aryaveer0121@gmail.com>
2025-08-10 10:53:57 +0530

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -267,4 +267,5 @@ window.viewReferralTree = async () => {
   }
 
   treeDiv.innerHTML = `<p>Referrals by ${uidText}:</p><ul>${refs.map(r => `<li>${r}</li>`).join('')}</ul>`;
+  <p>🎡 Unlocked: ${user.unlocked}</p>
 };
